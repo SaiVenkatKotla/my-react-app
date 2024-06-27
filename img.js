2b826a4db6c24e6c37fdbe07674f7ac44973a847
Sai Venkat Kotla <52619794+SaiVenkatKotla@users.noreply.github.com>
2024-06-27 12:13:37 +0530

    Add files via upload

---
+++ img.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const img = (props) => {
+  return (
+    <div style={{display:"inline"}}>
+      <img src= "props.src" height={150} width={150}/>
+    </div>
+  )
+}
+
+export default img
